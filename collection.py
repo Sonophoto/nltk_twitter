503983461dc24ee85c783b28d12a8ff2d76d0184
Ewan Klein <ewan@inf.ed.ac.uk>
2013-11-06 17:56:43 +1100

    Refactored credentials to util.py

--- collection.py
+++ collection.py
@@ -1,30 +1,20 @@
-from twython import Twython
+import os
 import json
+
+from twython import Twython, TwythonStreamer
+from util import credentials
 
 class Twitter:
 
     FILE ='tweets.json'
 
-    def authenticate(self, cred_fn=None):
-        creds = self.load_creds(cred_fn)
-        APP_KEY = creds['APP_KEY']
-        APP_SECRET = creds['APP_SECRET']
-        OAUTH_TOKEN = creds['OAUTH_TOKEN']
-        OAUTH_TOKEN_SECRET = creds['OAUTH_TOKEN_SECRET']   
-        self.twitter = Twython(APP_KEY, APP_SECRET,
-                      OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    def authenticate(self, cred_fn):
+        creds = credentials(cred_fn=cred_fn)
+        self.twitter = Twython(*creds)
 
-
-    def load_creds(self, cred_fn):
-        infile = open(cred_fn)
-        creds = json.load(infile)  
-        return creds
-    
-    
     def capture(self, fn=FILE, output=False):
         tweets = self.twitter.search(q='@OKFNAu')
-        outfile = open(fn, 'w')
-        
+        outfile = open(fn, 'w')       
         
         if output:
             print(json.dumps(tweets, sort_keys=True, indent=4, separators=(',', ': ')))
@@ -33,14 +23,17 @@ class Twitter:
                        
             
     def tweet_text(self, fn=FILE):
+        
         infile = open(fn)
         jsono = json.load(infile)
+        text = (st['text'] for st in jsono['statuses'] if ['text'] in st)
         
-        return jsono
+        outfn = os.path.splitext(fn)[0] + '.txt'
+        outfile = open(outfn, 'w')
+        for t in text: print(t, file=outfile)
+        outfile.close()
 
 tw = Twitter()   
 jsono = tw.tweet_text()
 
-text = (st['text'] for st in jsono['statuses'])
-for t in text: print(t)
     
