ef09b3a766eb268d7d533644127ed0fc2d461c52
Ewan Klein <ewan@inf.ed.ac.uk>
2013-11-13 14:27:17 +1100

    fixes to get streamer to write to file

--- streamer.py
+++ streamer.py
@@ -1,10 +1,34 @@
 from twython import TwythonStreamer
 from util import credentials
+import json
 
 class MyStreamer(TwythonStreamer):
+    #def on_success(self, data):
+        #if 'text' in data:
+            #print(data['text'])
+
     def on_success(self, data):
-        if 'text' in data:
-            print(data['text'])
+        count = 0
+        filename = 'collection'
+        output = open(filename + '.json', 'w')
+
+        while count < 5000:
+            if 'delete' in data:
+                pass
+            else:
+                status = json.dumps(data, indent=4) + '\n'
+                print(count)
+                output.write(status)
+                count += 1
+
+        self.disconnect()
+        output.close()
+
+    #def collect(self, n=10, filename):
+        #count = 0
+        #if self.on_success(data):
+
+
 
     def on_error(self, status_code, data):
         print(status_code)
@@ -14,8 +38,17 @@ class MyStreamer(TwythonStreamer):
         # self.disconnect()
     
     
-users = '@ianbaxter_green, @brtgrn, @GreenYes2014, @InappreciableT'
-tags = 'green edinburgh'
+users = [('ianbaxter_green','21085228'), ('brtgrn', '1625029752'),
+         ('GreenYes2014', '1530835490'), ('InappreciableT','774219451')]
+
+following = ', '.join([user[1] for user in users])
+
+tags = 'greens, edinburgh, twitter'
     
 stream = MyStreamer(*credentials('creds.json'))
-stream.statuses.filter(track=tags, follow=users)
+#stream.statuses.filter(track=tags, follow=following)
+
+stream.statuses.sample()
+
+
+    
